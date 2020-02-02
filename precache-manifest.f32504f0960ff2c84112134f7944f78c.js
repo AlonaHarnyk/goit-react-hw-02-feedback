606cdbbfbfce4781ff7f655fb4072739a8e785b1
Alona Harnyk <alyonagarnyk@gmail.com>
2020-02-02 14:51:26 +0200

    Updates

---
+++ precache-manifest.f32504f0960ff2c84112134f7944f78c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e0ca45a713f30361e566bcd14e5ff3",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "d892301beba8d76a9549",
+    "url": "/goit-react-hw-02-feedback/static/js/2.76181757.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/goit-react-hw-02-feedback/static/js/2.76181757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "060cf132799ea6834cba",
+    "url": "/goit-react-hw-02-feedback/static/js/main.72976200.chunk.js"
+  },
+  {
+    "revision": "123d81bea882563839a7",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.d87da01c.js"
+  }
+]);
